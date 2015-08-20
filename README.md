56170e5f155ca8d9ea8d0c9be49fdcb8b84e69ca
Naushad UzZaman <naushadzaman@gmail.com>
2015-08-20 12:50:21 -0400

    keyword extraction with parser

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # keyword-extraction-from-tweets
 keyword extraction from tweets using python 
+In this module, we use Pattern tools to do POS tagging/Phrase extraction of tweets. The usual POS tagging/chunking tools do not work well for free form texts like tweets, so we needed to use a tool that is designed and trained for twitter/tweets. From Pattern tool output, we extract phrases as entities. You can decide to use on NP (Noun Phrase), but our default is to use NP (Noun Phrase) and ADJP (Adjective Phrase). With this tool, you can also extract hashtags, usernames, urls from the tweet. 
+
+Questions, comments, feedbacks: n@uzzaman.com, @naushadzaman.
+
+
+# Install requirements (Pattern)
+Pattern: http://www.clips.ua.ac.be/pattern
+$ pip install -r requirements.txt
+
+# Examples
+$ python 
+
+## Sample keyword extraction
+>>> import keyword_extraction_w_parser
+>>> query = "The mobile web is more important than mobile apps."
+>>> keyword_extraction_w_parser.get_keywords(query)
+[u'mobile web', u'mobile apps', u'important']
+
+If we want to extract only the NP (Noun Phrase). 
+>>> keyword_extraction_w_parser.get_keywords(query, ['NP'])
+[u'mobile web', u'mobile apps']
+
+Another example: 
+>>> query = "As a #roadmapscholar, I highly recommend #startup bootcamp for #founders by @andrewsroadmaps : http://t.co/ZBISIMEBRH http://t.co/VF5CojRWNF"
+>>> keyword_extraction_w_parser.get_keywords(query)
+[u'roadmapscholar', u'startup bootcamp', u'founders']
+
+## Sample extraction of other entities 
+For tweets, hashtags, usernames, urls are obvious entities that we might need to extract. We provide that functionality as well. 
+
+Extract hashtags:
+>>> query = "As a #roadmapscholar, I highly recommend #startup bootcamp for #founders by @andrewsroadmaps : http://t.co/ZBISIMEBRH http://t.co/VF5CojRWNF"
+>>> keyword_extraction_w_parser.extract_hashtag(query) 
+['#roadmapscholar', '#startup', '#founders']
+
+Extract usernames (twitter handles): 
+>>> query = "RT @andrewsroadmaps: Proud of @naushadzaman &amp; @WasimKhal for winning the #IBMWatson hackathon! #roadmapscholars https://t.co/08sbAjKWKu"
+>>> keyword_extraction_w_parser.extract_users(query)
+['@andrewsroadmaps', '@naushadzaman', '@wasimkhal']
+
+Extract urls/links: 
+>>> query = "As a #roadmapscholar, I highly recommend #startup bootcamp for #founders by @andrewsroadmaps : http://t.co/ZBISIMEBRH http://t.co/VF5CojRWNF"
+>>> keyword_extraction_w_parser.extract_link(query)
+['http://t.co/ZBISIMEBRH', 'http://t.co/VF5CojRWNF']
+
+
---
+++ keyword_extraction_w_parser.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+__author__      = "Naushad UzZaman (@naushadzaman)"
+
+import os 
+import re 
+import sys 
+import subprocess
+
+
+from pattern.en import parse
+
+stopwords = [u'i', u'me', u'my', u'myself', u'we', u'our', u'ours', u'ourselves', u'you', u'your', u'yours', u'yourself', u'yourselves', u'he', u'him', u'his', u'himself', u'she', u'her', u'hers', u'herself', u'it', u'its', u'itself', u'they', u'them', u'their', u'theirs', u'themselves', u'what', u'which', u'who', u'whom', u'this', u'that', u'these', u'those', u'am', u'is', u'are', u'was', u'were', u'be', u'been', u'being', u'have', u'has', u'had', u'having', u'do', u'does', u'did', u'doing', u'a', u'an', u'the', u'and', u'but', u'if', u'or', u'because', u'as', u'until', u'while', u'of', u'at', u'by', u'for', u'with', u'about', u'against', u'between', u'into', u'through', u'during', u'before', u'after', u'above', u'below', u'to', u'from', u'up', u'down', u'in', u'out', u'on', u'off', u'over', u'under', u'again', u'further', u'then', u'once', u'here', u'there', u'when', u'where', u'why', u'how', u'all', u'any', u'both', u'each', u'few', u'more', u'most', u'other', u'some', u'such', u'no', u'nor', u'not', u'only', u'own', u'same', u'so', u'than', u'too', u'very', u's', u't', u'can', u'will', u'just', u'don', u'should', u'now']
+
+
+
+### START Keyword extraction ### 
+
+def extract_link(text):
+	regex = r'https?://[^\s<>"]+|www\.[^\s<>)"]+'
+	match = re.findall(regex, text)
+	return match
+	
+def cleanup(query): 
+	try:
+		urls = extract_link(" " + query + " ")
+		for url in urls: 
+			query = re.sub(url, "", query)
+		q = query.strip()
+	except:
+		q = query
+	q = re.sub(' RT ', '', ' ' + q + ' ').strip() 
+	return q 
+
+
+def convert_tag_format(query): 
+	word = query.split(' ')
+	postag = [(x.split('/')[0], x.split('/')[1]) for x in word]
+	return postag 
+	
+
+def get_pos_tags(text): 
+	tagged_sent = parse(text)	
+	return convert_tag_format(tagged_sent), tagged_sent
+	
+def normalise(word):
+	word = word.lower()
+	return word
+
+
+## conditions for acceptable word: length, stopword
+def acceptable_word(word):
+    accepted = bool(2 <= len(word) <= 40
+        and word.lower() not in stopwords)
+    return accepted
+
+## extract entity from BIO encoding 
+def extract_entity(filetext):
+	last_entity = '' 
+	last_tag = '' 
+	mention2entities = {} 
+	for line in filetext.split('\n'): 
+		line = line.strip() 
+		if line == '': 
+			continue
+		line_split = line.split('\t')
+		if re.search('B-', line_split[1]): 
+			if last_entity != '': 
+				if not last_tag in mention2entities:
+					mention2entities[last_tag] = [] 
+				mention2entities[last_tag].append(last_entity.strip())
+			last_entity = line_split[0] + ' '
+			last_tag = line_split[1][2:] 
+		elif re.search('I-', line_split[1]): 
+			last_entity += line_split[0] + ' '
+	if last_entity != '': 
+		if not last_tag in mention2entities:
+			mention2entities[last_tag] = [] 
+		mention2entities[last_tag].append(last_entity.strip())
+	return 	mention2entities
+
+	
+def get_entities_from_phrase(tagged_sent, phrase2consider): 
+	word = tagged_sent.split(' ')
+	bio_tags = [normalise(x.split('/')[0])+ '\t'+ x.split('/')[2] for x in word]
+	bio_text = '\n'.join(bio_tags)
+	mention2entities = extract_entity(bio_text)
+	
+	## strip off unacceptable words 
+	_mention2entities = {} 
+	for mention in mention2entities: 
+		if not mention in phrase2consider: 
+			continue
+		_mention2entities[mention] = [] 
+		for entity in mention2entities[mention]: 
+			_entity = ' '.join([word for word in entity.split(' ') if acceptable_word(word)]).strip()
+			if _entity != '': 
+				_mention2entities[mention].append(_entity)
+			
+	entities = []
+	for mention in _mention2entities: 
+		entities.extend(_mention2entities[mention])
+	return entities	
+	
+
+def get_keywords(text, phrase2consider=['NP', 'ADJP']): 
+	_text = cleanup(text)
+	postoks, tagged_sent = get_pos_tags(_text)
+	entities = get_entities_from_phrase(tagged_sent, phrase2consider)
+	return entities
+
+### END Keyword extraction ### 
+
+### START other entity extraction ### 
+def extract_hashtag(text, to_normalize=True):
+	regex = r'#[^\W\d_]+\b'
+	if to_normalize: text = normalise(text)
+	match = re.findall(regex, text)
+	return match
+
+def extract_users(text, to_normalize=True):
+	regex = r'@[^\W\d_]+\b'
+	if to_normalize: text = normalise(text)
+	match = re.findall(regex, text)
+	return match
+
+### END other entity extraction ### 
+	
+	
+if __name__ == '__main__':	
+	queries = ["The mobile web is more important than mobile apps.", "As a #roadmapscholar, I highly recommend #startup bootcamp for #founders by @andrewsroadmaps : http://t.co/ZBISIMEBRH http://t.co/VF5CojRWNF", "RT @andrewsroadmaps: Proud of @naushadzaman &amp; @WasimKhal for winning the #IBMWatson hackathon! #roadmapscholars https://t.co/08sbAjKWKu"]
+	
+	for query in queries: 
+		print 'query', query 
+		print 'get_keywords(query)', get_keywords(query)
+		print "get_keywords(query, ['NP'])", get_keywords(query, ['NP'])
+		print "extract_hashtag(query)", extract_hashtag(query) 
+		print "extract_users(query)", extract_users(query)
+		print "extract_link(query)", extract_link(query)
+		print ''
